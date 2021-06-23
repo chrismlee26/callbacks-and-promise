3a6e72a887d0840bf7d51a7c4c759d6434e73c1b
CHRIS_LEE <chrismlee87@gmail.com>
2021-06-23 10:36:41 -0700

    complete

--- Promise-6/index.js
+++ Promise-6/index.js
@@ -1,11 +1,27 @@
 function whatToEat(timeOfDay) {
   // Create a new promise and return it here
-  
+  return new Promise((resolve, reject) => {
+    if (timeOfDay === 'breakfast') {
+      resolve('Jian Bing')
+    }
+    if (timeOfDay === 'lunch') {
+      resolve('Salad')
+    }
+    if (timeOfDay === 'dinner') {
+      resolve('Steak')
+    }
+      reject('I\'m not hungry right now.')
+  })
 }
 
-
 // Call yout function and resolve the promise here! 
-whatToEat('lunch')
+whatToEat('lunch').then((n) => {
+  console.log(n)
+})
+.catch((err) => {
+  console.log(err)
+})
+.finally(console.log('enjoy!'))
 
 
 // If the `timeOfDay` is 'breakfast', 'lunch', or 'dinner' 
--- Promise-6/readme.md
+++ Promise-6/readme.md
@@ -1,6 +1,6 @@
 # Promise Finally
 
-Sometime you'll want to run some code when a promise settles, that is the promise has resolved or rejected. You can use finally to do this. For Example: 
+Sometime you'll want to run some code when a promise settles, that is the promise has resolved or rejected. You can use finally to do this. For Example:
 
 ```JS
 const p = new Promise(...)
@@ -9,4 +9,4 @@ p.catch() // runs when the promise rejects
 p.finally() // runs when the promise settles (either resolved or rejected)
 ```
 
-Quick review: 
+Quick review:
